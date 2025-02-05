83dd0a6059f6be95e6965a2e904cd52bd48ec2a3
Leo Thomas <leothomas0095@gmail.com>
2025-02-05 11:02:06 +0530

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'algebra1',
-  host: 'myorder-db.xyz123.region.rds.amazonaws.com',
+  host: 'algebra1.ctqaoyew8pt1.ap-south-1.rds.amazonaws.com',
   database: 'postgres',
   password: 'Merinjai',
   port: 5432, // Default PostgreSQL port
