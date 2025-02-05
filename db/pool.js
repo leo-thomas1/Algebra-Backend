7310c253f237509e82fd6e46181410301af80317
Leo Thomas <leothomas0095@gmail.com>
2025-02-05 09:49:53 +0530

    do

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'db_test',
-  password: 'leo@123',
+  user: 'algebra1',
+  host: 'myorder-db.xyz123.region.rds.amazonaws.com',
+  database: 'postgres',
+  password: 'Merinjai',
   port: 5432, // Default PostgreSQL port
 });
 module.exports = pool;
